de9e408ed681ee0cf088a33ea960f04d5dbc3bf1
xymerz1 <xymerz1@gmail.com>
2019-07-13 16:46:38 +0800

    Building formula

--- reinforce-tf/main.py
+++ reinforce-tf/main.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import random
+import numpy as np
 
 file_name = "EURUSD43200.csv"
 data = pd.read_csv(file_name)
@@ -30,3 +31,28 @@ class RandomDecisionPolicy(DecisionPolicy):
 
             return action
 
+def run_simulation(policy, initial_budget, initial_entry_number_buy, initial_entry_number_sell, prices, hist, debug=False):
+    budget = initial_budget
+    num_of_entry_buy = initial_entry_number_buy
+    num_of_entry_sell = initial_entry_number_sell
+    pair_value = 0
+    pips = 0
+    sell_price = 0
+    buy_price = 0
+    current_portfolio = 0
+    transitions = list()
+    for i in range(len(prices) - hist - 1):
+        if i % 100 == 0:
+            print('progress {:.2f}%'.format(float(100*i) / (len(prices) - hist - 1)))
+            current_state = np.asmatrix(np.hstack((prices[i:i+hist], budget, num_of_entry_buy, num_of_entry_sell)))
+            if num_of_entry_buy == 1:
+                if sell_price > buy_price:
+                    # calculate the profit
+                    pip = (sell_price - buy_price) * 1000
+                    pip_m = pip * 1
+                    current_portfolio = budget + pip_m
+                else:
+                    #calculate the loss
+                    pip = (sell_price - buy_price) * 1000
+                    pip_m = pip * 1
+                    current_portfolio = budget - pip_m
